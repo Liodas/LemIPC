1972282b15b1464b8b2c3045d689fb85a3dae835
Remi Gastaldi <remi.gastaldi@epitech.eu>
2017-03-27 14:39:30 +0200

    Rename destrucs -> core

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login	gastal_r
 **
 ** Started on	Sun Mar 26 12:28:14 2017 gastal_r
-** Last update	Sun Mar 26 12:43:08 2017 gastal_r
+** Last update	Mon Mar 27 14:39:16 2017 gastal_r
 */
 
 #include      "lemipc.h"
@@ -56,39 +56,39 @@ int		*initMap(int *map)
   return (map);
 }
 
-int		initSem(t_struct *desTrucs)
+int		initSem(t_struct *core)
 {
-  if ((desTrucs->semId = semget(desTrucs->key, 1, IPC_CREAT | SHM_R | SHM_W)) == -1) /* get id ensemble semaphores OU en créée si inexistants */
+  if ((core->semId = semget(core->key, 1, IPC_CREAT | SHM_R | SHM_W)) == -1) /* get id ensemble semaphores OU en créée si inexistants */
     return (fprintf(stderr, "Semget failed\n") - 15);
-  semctl(desTrucs->semId, 0, SETVAL, 1); /* SETVAL 1 sur semaphore 0 */
+  semctl(core->semId, 0, SETVAL, 1); /* SETVAL 1 sur semaphore 0 */
   return (0);
 }
 
-int		initMsg(t_struct *desTrucs)
+int		initMsg(t_struct *core)
 {
-  if ((desTrucs->msgId = msgget(desTrucs->key, SHM_R | SHM_W)) == -1) /* get id file de messagesQ  */
-    if ((desTrucs->msgId = msgget(desTrucs->key, IPC_CREAT | SHM_R | SHM_W)) == -1)  /* créé nouvelle file de messagesQ si inexistante */
+  if ((core->msgId = msgget(core->key, SHM_R | SHM_W)) == -1) /* get id file de messagesQ  */
+    if ((core->msgId = msgget(core->key, IPC_CREAT | SHM_R | SHM_W)) == -1)  /* créé nouvelle file de messagesQ si inexistante */
       return (fprintf(stderr, "Msgget failed\n") - 15);
   return (0);
 }
 
-int		initValues(t_struct *desTrucs, char *path)
+int		initValues(t_struct *core, char *path)
 {
-  if ((desTrucs->key = ftok(path, 0)) == -1) /* get key */
+  if ((core->key = ftok(path, 0)) == -1) /* get key */
     return (print_usage());
-  if ((desTrucs->shmId = shmget(desTrucs->key, sizeof(int **) * (50 * 50), SHM_R | SHM_W)) == -1) /* alloc segment mem partagée */
+  if ((core->shmId = shmget(core->key, sizeof(int **) * (50 * 50), SHM_R | SHM_W)) == -1) /* alloc segment mem partagée */
     {
-      if ((desTrucs->shmId = shmget(desTrucs->key, sizeof(int **) * (50 * 50), IPC_CREAT | SHM_R | SHM_W)) != -1) /* alloc segment mem partagée si segment pas créé */
+      if ((core->shmId = shmget(core->key, sizeof(int **) * (50 * 50), IPC_CREAT | SHM_R | SHM_W)) != -1) /* alloc segment mem partagée si segment pas créé */
 	{
-	  if ((desTrucs->addr = shmat(desTrucs->shmId, NULL, SHM_R | SHM_W)) == (void *)-1) /* attache la mem partagée au processus  */
+	  if ((core->addr = shmat(core->shmId, NULL, SHM_R | SHM_W)) == (void *)-1) /* attache la mem partagée au processus  */
 	    return (fprintf(stderr, "Shmat failed\n") - 14);
 	  else
 	    {
-	      if (initMsg(desTrucs) == -1
-		  || initSem(desTrucs) == -1)
+	      if (initMsg(core) == -1
+		  || initSem(core) == -1)
 		return (-1);
-	      desTrucs->addr = initMap(desTrucs->addr);
-	      /* displayMap(desTrucs->addr); */
+	      core->addr = initMap(core->addr);
+	      /* displayMap(core->addr); */
 	      /* TODO : -Créer pion
 		        -Fork entre affichage map && reste
 			-reste = Déplacement - check si mort */
@@ -101,12 +101,12 @@ int		initValues(t_struct *desTrucs, char *path)
 
 int		main(int ac, char *av[])
 {
-  t_struct	desTrucs;
+  t_struct	core;
 
   if (ac != 3)
     return (print_usage());
-  if (initValues(&desTrucs, av[1]) == -1)
+  if (initValues(&core, av[1]) == -1)
     return (-1);
-  printf("Key = %d\nShmId = %d\nAddr = %lld\nmsgId = %d\n", desTrucs.key, desTrucs.shmId, (long long int)desTrucs.addr, desTrucs.msgId);
+  printf("Key = %d\nShmId = %d\nAddr = %lld\nmsgId = %d\n", core.key, core.shmId, (long long int)core.addr, core.msgId);
   return (0);
 }
